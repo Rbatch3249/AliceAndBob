fbd12fe2c8197e89e8a268dad2a4e9990e24a0e6
Ryan <rbatchelor3249@gmail.com>
2020-09-15 12:40:37 -0400

    Alic and bob

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,29 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args ){
+    public static void main(String[] args) {
+        System.out.println("Enter your name:");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
 
+
+        boolean isNameAliceOrBob = true;
+        while(isNameAliceOrBob )
+        if (name.equalsIgnoreCase("Alice") || name.equalsIgnoreCase("Bob")) {
+            isNameAliceOrBob = false;
+            System.out.println("Greetings");
+            break;
+            }else{
+                System.out.println("Invalid Name");
+                name = scanner.next();
+
+        }
+
+            }
     }
-}
+
